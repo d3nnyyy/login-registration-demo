ecf989fdec6941b50dc02d72b6796b5e1718c848
d3nnyyy <d3nnn41k@gmail.com>
2023-08-05 22:21:30 +0300

    added Role to RegistrationRequest

--- backend/src/main/java/ua/dtsebulia/backend/auth/RegistrationRequest.java
+++ backend/src/main/java/ua/dtsebulia/backend/auth/RegistrationRequest.java
@@ -4,16 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ua.dtsebulia.backend.user.Role;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class RegisterRequest {
+public class RegistrationRequest {
 
     private String firstname;
     private String lastname;
     private String email;
     private String password;
+    private Role role;
 
 }
