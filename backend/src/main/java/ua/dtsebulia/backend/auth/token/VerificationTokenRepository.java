556f8376e5ccc7d3bd32ef576bd23abc05c49f97
d3nnyyy <d3nnn41k@gmail.com>
2023-08-05 22:11:41 +0300

    added verification token with its repo

---
+++ backend/src/main/java/ua/dtsebulia/backend/auth/token/VerificationTokenRepository.java
@@ -0,0 +1,7 @@
+package ua.dtsebulia.backend.auth.token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    VerificationToken findByToken(String token);
+}
