ecced73122164ec3a7d4b493c551d5d29c8dc40f
d3nnyyy <d3nnn41k@gmail.com>
2023-08-07 13:16:26 +0300

    added logging to controller, also created a good exception handling for password reset methods

--- backend/src/main/java/ua/dtsebulia/backend/auth/AuthenticationController.java
+++ backend/src/main/java/ua/dtsebulia/backend/auth/AuthenticationController.java
@@ -3,11 +3,15 @@ package ua.dtsebulia.backend.auth;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 import ua.dtsebulia.backend.auth.password.PasswordResetRequest;
 import ua.dtsebulia.backend.auth.password.PasswordResetService;
 import ua.dtsebulia.backend.auth.token.ResendVerificationTokenService;
+import ua.dtsebulia.backend.exception.InvalidTokenException;
+import ua.dtsebulia.backend.exception.PasswordResetException;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -25,40 +29,80 @@ public class AuthenticationController {
     public ResponseEntity<?> register(
             @RequestBody RegistrationRequest registrationRequest,
             HttpServletRequest request) {
-        return ResponseEntity.ok(authenticationService.register(registrationRequest, request));
+        try {
+            return ResponseEntity.ok(authenticationService.register(registrationRequest, request));
+        } catch (Exception e) {
+            log.error("Error during registration: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred during registration.");
+        }
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(
             @RequestBody AuthenticationRequest authenticationRequest) {
-        return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+        try {
+            return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+        } catch (Exception e) {
+            log.error("Authentication error: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failed.");
+        }
     }
 
     @GetMapping("/verifyEmail")
     public ResponseEntity<?> verifyEmail(
             @RequestParam("token") String token) {
-        return ResponseEntity.ok(emailVerificationService.verifyEmail(token));
+        try {
+            return ResponseEntity.ok(emailVerificationService.verifyEmail(token));
+        } catch (Exception e) {
+            log.error("Email verification error: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email verification failed.");
+        }
     }
 
     @GetMapping("/resend-verification-token")
     public ResponseEntity<?> resendVerificationToken(
             @RequestParam("token") String oldToken,
             HttpServletRequest request) {
-        return ResponseEntity.ok(resendVerificationTokenService.resendVerificationToken(oldToken, request));
+        try {
+            return ResponseEntity.ok(resendVerificationTokenService.resendVerificationToken(oldToken, request));
+        } catch (Exception e) {
+            log.error("Resend verification token error: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while resending verification token.");
+        }
     }
 
     @PostMapping("/password-reset-request")
-    public ResponseEntity<?> resetPasswordRequest(
+    public ResponseEntity<String> resetPasswordRequest(
             @RequestBody PasswordResetRequest passwordResetRequest,
             HttpServletRequest request) {
-        return ResponseEntity.ok(passwordResetService.resetPasswordRequest(passwordResetRequest, request));
+        try {
+            String result = passwordResetService.resetPasswordRequest(passwordResetRequest, request);
+            return ResponseEntity.ok(result);
+        } catch (UsernameNotFoundException e) {
+            log.error("Username not found: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        } catch (Exception e) {
+            log.error("Password reset request error: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred during password reset request.");
+        }
     }
 
     @PostMapping("/reset-password")
-    public ResponseEntity<?> resetPassword(
+    public ResponseEntity<String> resetPassword(
             @RequestBody PasswordResetRequest passwordResetRequest,
             @RequestParam("token") String passwordResetToken) {
-        return ResponseEntity.ok(passwordResetService.resetPassword(passwordResetRequest, passwordResetToken));
+        try {
+            String result = passwordResetService.resetPassword(passwordResetRequest, passwordResetToken);
+            return ResponseEntity.ok(result);
+        } catch (InvalidTokenException e) {
+            log.error("Invalid password reset token: {}", e.getMessage());
+            return ResponseEntity.badRequest().body("Invalid password reset token.");
+        } catch (PasswordResetException e) {
+            log.error("Password reset error: {}", e.getMessage());
+            return ResponseEntity.badRequest().body("Password reset failed.");
+        } catch (Exception e) {
+            log.error("Unexpected error during password reset: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred during password reset.");
+        }
     }
-
 }
